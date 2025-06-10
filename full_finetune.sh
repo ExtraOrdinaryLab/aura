3a98e011faa3fd7e54196270be1ea7cd51e888d6
penguinwang96825 <penguinwang@smail.nchu.edu.tw>
2025-06-10 11:45:22 +0000

    [feat] update args

--- full_finetune.sh
+++ full_finetune.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-MODEL_ID="openai/whisper-tiny"
-TRAIN_MANIFEST="/home/jovyan/workspace/aura/manifest/fisher_segments.jsonl"
-TEST_MANIFEST="/home/jovyan/workspace/aura/manifest/eval2000.jsonl"
+MODEL_ID="openai/whisper-medium"
+TRAIN_MANIFEST="/home/jovyan/workspace/aura/manifest/sap_train.jsonl"
+TEST_MANIFEST="/home/jovyan/workspace/aura/manifest/sap_dev.jsonl"
 
-LEARNING_RATE="1e-4"
+LEARNING_RATE="5e-6"
 
 OUTPUT_DIR="output/full/sap"
-
-CUDA_VISIBLE_DEVICES=0 python lora_finetune.py \
+    
+CUDA_VISIBLE_DEVICES=0 python full_finetune.py \
     --train_data $TRAIN_MANIFEST \
     --test_data $TEST_MANIFEST \
     --base_model $MODEL_ID \
@@ -17,10 +17,12 @@ CUDA_VISIBLE_DEVICES=0 python lora_finetune.py \
     --eval_steps 1000 \
     --max_steps 10000 \
     --warmup_steps 500 \
-    --lora_type lora \
     --language English \
     --task transcribe \
     --per_device_train_batch_size 16 \
     --gradient_accumulation_steps 4 \
     --output_dir $OUTPUT_DIR \
+    --save_total_limit 1 \
+    --report_to "none" \
     --push_to_hub False
+    
