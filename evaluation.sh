ce7514b12f25ec0924a6a41f1b5d511c9187d085
penguinwang96825 <penguinwang@smail.nchu.edu.tw>
2025-06-10 11:50:39 +0000

    [update] loop through models and test data

--- evaluation.sh
+++ evaluation.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-MODEL_PATH="/path/to/checkpoint"
-TEST_DATA="/path/to/SAP/dev.v2.jsonl"
+MODEL_PATHS=(
+    "models/lora/sap/whisper-tiny-finetune"
+)
 
-python evaluation.py \
-    --test_data $TEST_DATA \
-    --model_path $MODEL_PATH
+TEST_MANIFESTS=(
+    "/home/jovyan/workspace/aura/manifest/test_clean.jsonl"
+    "/home/jovyan/workspace/aura/manifest/test_other.jsonl"
+    "/home/jovyan/workspace/aura/manifest/atypical_severe.jsonl"
+    "/home/jovyan/workspace/aura/manifest/atypical_moderate.jsonl"
+    "/home/jovyan/workspace/aura/manifest/atypical_mild.jsonl"
+    "/home/jovyan/workspace/aura/manifest/dev.v2.jsonl"
+)
+
+# Loop through each model and test data
+for MODEL_PATH in "${MODEL_PATHS[@]}"; do
+    for TEST_MANIFEST in "${TEST_MANIFESTS[@]}"; do
+        echo "Evaluating $MODEL_PATH with $TEST_MANIFEST"
+        python evaluation.py \
+            --test_data_path "$TEST_MANIFEST" \
+            --model_path "$MODEL_PATH"
+    done
+done
